3ef3795654ca747b23c9af1536acbdc2eb50ff30
ajbates2 <60674015+ajbates2@users.noreply.github.com>
2020-04-09 14:10:04 -0500

    yelp doesnt accept cors, swithced to a brewery database, working on map functionality

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const yelpKey = 'bearer xayaX2zcNWshNIUUXWL1ejHLyjysVwNRV9VrBaW-gWDcx8IpE4awC6eFudFVjscmJUKwJdPSiifHVSrg_pkjh79BSnRNLe6-VPAWttmnw_6jAFSzkonBZRtnqCuOXnYx'; 
-const clientID = 'bwYJJmIW6MYYSCklgSIFsQ'
-const searchURL = 'https://api.yelp.com/v3/businesses/search?categories=ramen';
-
+const searchURL = 'https://api.openbrewerydb.org/breweries';
+const mapsKey = 'AIzaSyAe4FTwPUmFTXLaXAqMZCGEHX1r-ZHs5LU'
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -12,31 +10,27 @@ function formatQueryParams(params) {
 function displayResults(responseJson) {
   console.log(responseJson);
   $('#results-list').empty();
-  for (let i = 0; i < responseJson.businesses.length; i++){
+  for (let i = 0; i < responseJson.length; i++){
     $('#results-list').append(
-      `<li><h3>${responseJson.businesses[i].name}</h3>
-      <p>${responseJson.businesses[i].rating}</p>
-      <a href='${responseJson.businesses[i].url}>website</a>'>
+      `<li><h3>${responseJson[i].name}</h3>
+      <p>${responseJson[i].brewery_type}</p>
+      <a href="${responseJson[i].website_url}">website</a>
       </li>`
-    )}; 
+    )};
   $('#results').removeClass('hidden');
 };
 
 function getResource(searchTerm) {
   const params = {
-    location: searchTerm
+    by_city: searchTerm
   };
   const queryString = formatQueryParams(params)
-  const url = searchURL + '&' + queryString;
+  const url = searchURL + '?' + queryString;
   console.log(url);
-  const options = {
-    mode: "no-cors",
-    headers: new Headers({
-      "Authorization": yelpKey})};
 
-  fetch(url, options)
+  fetch(url)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
+    .then(responseJson => placeResource(responseJson))
     .catch(error => console.log(error.message));
 }
 
@@ -49,3 +43,22 @@ function watchForm() {
 }
 
 $(watchForm);
+
+function initMap() {
+  let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 44.977, lng: -93.265},
+    zoom: 12
+  });
+}
+
+function placeResource(responseJson) {
+  for (let i = 0; i < responseJson.length; i++){
+    let coords = [responseJson[i].latitude, responseJson[i].longitude];
+    console.log(coords);
+    let latLng = new google.maps.LatLng(coords);
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+});
+}}
+
